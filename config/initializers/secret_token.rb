dcce20cab840adb47f8ec33a86ed8db173f31871
David Vega <david.vega@crowdint.com>
2013-01-14 22:19:25 -0600

    Movement Detector Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovementDetector::Application.config.secret_token = '2431b08a7daa7af581d9d86037b97d58f7e15bef4642d5b23d9dadc3ae6d1f46f742fd0e3b5f814ddd0dd3a8b22235836fd54da6b9ed0585847dd51025314965'
